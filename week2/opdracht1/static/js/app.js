01db02bba8b6d344983f89f641d360ec2170a391
jaimiederijk <j.r.derijk@quicknet.nl>
2016-02-12 13:56:21 +0100

    movie and dot for iss

--- week2/opdracht1/static/js/app.js
+++ week2/opdracht1/static/js/app.js
@@ -4,12 +4,23 @@
 	var app = {
 		init: function() {
 			routes.init();
-
+			googleMap.setupMap()
+			sections.refreshIssMarker()
 		}
 	};
 
 	var routes = {
 		init: function() {
+			routie({
+			    'home': function() {
+			      
+			    },
+			    'who': function() {
+			      
+			    },
+			    'why': function() {
+			    }
+			});
 			var currentHash = window.location.hash;
 			var hash = currentHash ? currentHash : "#start";//if there is a hash stay there else go to start
 			sections.toggle(hash);
@@ -29,23 +40,21 @@
 
 			matchingSection.classList.remove("hidden");//show the matching section
 
+		},
+		refreshIssMarker : function () {
+			var markerInterval = window.setInterval(call ,2000);
+			function call () {
+				data.requestDataIss(data.baseUrl+"satellites/25544","issData")
+			}
 		}
 	};
 
 	var data = {
-		baseUrl:"https://api.wheretheiss.at/v1/",
+		issBaseUrl:"https://api.wheretheiss.at/v1/",
+		movieBaseUrl:"https://api.themoviedb.org/3/",
+		apiKey:"6cb38eaa7fe8c8602cce052374cdf3ad",
 
-		// requestDataPeg: function(url) {
-
-		// 	pegasus(url).then(
-		// 		function (data,xhr) {
-		// 			return data
-		// 		}, function (data,xhr) {
-		// 			console.error(data, xhr.status)
-		// 		}
-		// 	);
-		// },
-		requestData:function (url,target) {
+		requestDataIss:function (url,target) {
 			var self = this;
 			var target = target;
 			promise.get(url).then(function(error, text, xhr) {
@@ -55,48 +64,68 @@
 			    }
 			    self[target] = JSON.parse(text);
 			    //googleMap.setMapCordinates();
-
-			    googleMap.setupMap()
+			    googleMap.setupMarker(target);
+			    
 			});
 			//debugger
+		},
+		searchMovie :function(input) {
+			var url = this.movieBaseUrl + 'search/multi?query=' + input +'?api_key=' + this.apiKey;
 
+			promise.get(url).then(function(error, text, xhr) {
+				if (error) {
+			        alert('Error ' + xhr.status);
+			        return;
+			    }
+			    console.log(JSON.parse(text));
+			});
 		}
 	};
 
+	var map;
+
 	var googleMap = {
-		setMapCordinates : function() {
-			
-			this.myLatLng.lat = data.issData.latitude;
-			this.myLatLng.lng = data.issData.longitude;
+		getCordinates : function(target) {
+			var myLatLng = {
+			      lat: data[target].latitude,
+			      lng: data[target].longitude
+			};
+			return myLatLng
 		},
 		setupMap : function () {
 			if (document.getElementById('map-canvas')){
  
 			    // Coordinates to center the map
-			    var myLatlng = new google.maps.LatLng(data.issData.latitude,data.issData.longitude);
+			    var myLatlng = new google.maps.LatLng(0,0);
 			 
 			    // Other options for the map, pretty much selfexplanatory
 			    var mapOptions = {
-			        zoom: 3,
+			        zoom: 1,
 			        center: myLatlng,
 			        mapTypeId: google.maps.MapTypeId.ROADMAP
 			    };
-			    var myLatLng = {
-			      lat: data.issData.latitude,
-			      lng: data.issData.longitude
-			    };
-			  
-			    // Attach a map to the DOM Element, with the defined settings
-			    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-			}
 
+			    // Attach a map to the DOM Element, with the defined settings
+				map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions )
+			    
+			}
+		},
+		setupMarker : function (target) {
+			var image = {
+				url: "static/images/dot.png",
+			    scaledSize: new google.maps.Size(20, 20), // scaled size
+			    origin: new google.maps.Point(0,0), // origin
+			    anchor: new google.maps.Point(0, 0) // anchor
+			}
 
 			var marker = new google.maps.Marker({
-			    position: myLatLng,
+			    position: this.getCordinates(target),
 			    map: map,
-			    title: 'Bijna'
-			    });
-			}
+			    title: target,
+			    icon: image
+			});
+		}
+			
 		// myLatlng : new google.maps.LatLng(52.3661379,4.9160171),// Coordinates to center the map
 		// mapOptions : {// Other options for the map, pretty much selfexplanatory   debugger
 
@@ -120,7 +149,7 @@
 
 	app.init();
 
-	data.requestData(data.baseUrl+"satellites/25544","issData");
+	
 
 
 	//var testData = data.requestDataPeg('https://api.wheretheiss.at/v1/' + 'satellites/25544');
