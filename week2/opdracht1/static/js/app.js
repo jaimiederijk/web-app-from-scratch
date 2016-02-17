aaedd07497a6760674c1da4615ec49aae8b31cc5
jaimiederijk <j.r.derijk@quicknet.nl>
2016-02-17 13:50:21 +0100

    week2

--- week2/opdracht1/static/js/app.js
+++ week2/opdracht1/static/js/app.js
@@ -1,73 +1,140 @@
 'use strict';
-(function(){
+//(function(){
+
+	var htmlElements = {
+		sections: document.querySelectorAll('section'),
+		movies: document.querySelector('#searchmovies'),
+		movie: document.querySelector('#onemovie'),
+		home: document.querySelector('#start'),
+		iss: document.querySelector('#isstracker')
+	};
 
 	var app = {
 		init: function() {
+			// sections.firstHideAllSections();
 			routes.init();
-			googleMap.setupMap()
-			sections.refreshIssMarker()
+			googleMap.setupMap();
+			sections.refreshIssMarker.markerInterval();
 		}
 	};
 
 	var routes = {
 		init: function() {
-			var currentHash = window.location.hash;
-			var hash = currentHash ? currentHash : "#start";//if there is a hash stay there else go to start
-			sections.toggle(hash);
-			window.addEventListener('hashchange', function(){sections.toggle()} ,false);//event hashchange fires sections.toggle
+			routie('', function() {
+				sections.displaySection("home");
+			});
+			routie('movies', function() {
+				sections.displaySection("movies");
+				document.querySelector("#searchmovies form").addEventListener("submit", function () {
+					sections.setupMovieSearched(event.target[0].value);
+				});//
+			});
+			routie('iss', function() {
+				sections.displaySection("iss");
+			});
+			routie('movies/:id', function(id) {
+				if (data.searchedMovies) {//has data been collected then continue
+					sections.displaySection("movie");
+					sections.renderMoviePage(id);
+				} else {
+					routie('movies');
+				};
+			});
 		}
+
 	};
 
 	var sections = {
-		toggle: function(route) {
-			var id = route ? route : window.location.hash; //if there is a route take that one else current hash
-			var sections = document.querySelectorAll("section");
-			var matchingSection = document.querySelector(id);
+		setupMovieSearched : function (input) {
+			if (data.searchedMovies) {
 
-			for (var i = 0; i < sections.length; i++) { //hide all sections
-				sections[i].classList.add("hidden");
-			};
-
-			matchingSection.classList.remove("hidden");//show the matching section
-
+				data.oldSearchedMovies =_.filter(data.searchedMovies, function(vote_average){
+					return vote_average > 5.5;
+				});
+			}
+			data.searchMovie(input,"searchedMovies");
 		},
-		refreshIssMarker : function () {
-			var markerInterval = window.setInterval(call ,2000);
-			function call () {
-				data.requestDataIss(data.issBaseUrl+"satellites/25544","issData")
+		renderMovieSearched : function () {
+			var temp = document.querySelector("#template");
+			var movies = data.searchedMovies;
+			var directives = {
+				deeplink : {
+					href : function (params) {
+						return "#movies/#" + this.id;
+					}
+				}
+			};
+			Transparency.render(temp,movies.results,directives);
+		},
+		renderMoviePage : function (id) {
+			var movie = _.find(data.searchedMovies.results,function(id){ return id = id; });
+			var temp = document.querySelector("#template-movie");
+			Transparency.render(temp,movie);
+		},
+		// firstHideAllSections : function() {
+		// 	var sections = htmlElements.sections;
+		// 	for (var i = 0; i < sections.length; i++) { //hide all sections via loop
+		// 		sections[i].classList.add("notransition");
+		// 	};
+		// },
+		hideAllSections : function () {
+			var sections = htmlElements.sections;
+			for (var i = 0; i < sections.length; i++) { //hide all sections via loop
+
+				sections[i].classList.add("hidden");
+				//sections[i].classList.remove("notransition");
+			};
+		},
+		displaySection : function (sectionName) {
+			this.hideAllSections();
+			var section = htmlElements[sectionName];
+			section.classList.remove("hidden");
+		},
+		refreshIssMarker : {
+			markerInterval : function() {this.interval = window.setInterval(this.sendIssRequest ,2000)},
+			sendIssRequest : function() {
+				data.requestDataIss(data.config.issBaseUrl+"satellites/25544","issData");
+
+			},
+			stopInterval : function() {
+				clearInterval(this.interval);
 			}
 		}
 	};
 
 	var data = {
-		issBaseUrl:"https://api.wheretheiss.at/v1/",
-		movieBaseUrl:"https://api.themoviedb.org/3/",
-		apiKey:"6cb38eaa7fe8c8602cce052374cdf3ad",
+		config : {
+			issBaseUrl:"https://api.wheretheiss.at/v1/",
+			movieBaseUrl:"http://api.themoviedb.org/3/",
+			movieApiKey:"3974f78e9e581f953c413271e51a527a",
+		},
 
-		requestDataIss:function (url,target) {
+		requestDataIss:function (url, target) {	//target = under what name should the the data be saved
 			var self = this;
 			var target = target;
 			promise.get(url).then(function(error, text, xhr) {
 			    if (error) {
+			    	sections.refreshIssMarker.stopInterval();
 			        alert('Error ' + xhr.status);
 			        return;
 			    }
 			    self[target] = JSON.parse(text);
 			    //googleMap.setMapCordinates();
 			    googleMap.setupMarker(target);
-			    
 			});
 			//debugger
 		},
-		searchMovie :function(input) {
-			var url = 'https://api.themoviedb.org/3/' + 'search/multi?query=' + input +'?api_key=6cb38eaa7fe8c8602cce052374cdf3ad';
-
+		searchMovie :function(input, target) {	//target = under what name should the the data be saved
+			var url = this.config.movieBaseUrl + 'search/multi?query=' + input +'&api_key=' + this.config.movieApiKey;
+			var self = this;
 			promise.get(url).then(function(error, text, xhr) {
 				if (error) {
 			        alert('Error ' + xhr.status);
 			        return;
-			    }
-			    console.log(JSON.parse(text));
+			    };
+			    self[target] = JSON.parse(text);
+			    sections.renderMovieSearched();
+			    //console.log(JSON.parse(text));
 			});
 		}
 	};
@@ -84,10 +151,10 @@
 		},
 		setupMap : function () {
 			if (document.getElementById('map-canvas')){
- 
+
 			    // Coordinates to center the map
 			    var myLatlng = new google.maps.LatLng(0,0);
-			 
+
 			    // Other options for the map, pretty much selfexplanatory
 			    var mapOptions = {
 			        zoom: 1,
@@ -96,8 +163,8 @@
 			    };
 
 			    // Attach a map to the DOM Element, with the defined settings
-				map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions )
-			    
+				map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions );
+
 			}
 		},
 		setupMarker : function (target) {
@@ -106,7 +173,7 @@
 			    scaledSize: new google.maps.Size(20, 20), // scaled size
 			    origin: new google.maps.Point(0,0), // origin
 			    anchor: new google.maps.Point(0, 0) // anchor
-			}
+			};
 
 			var marker = new google.maps.Marker({
 			    position: this.getCordinates(target),
@@ -115,34 +182,12 @@
 			    icon: image
 			});
 		}
-			
-		// myLatlng : new google.maps.LatLng(52.3661379,4.9160171),// Coordinates to center the map
-		// mapOptions : {// Other options for the map, pretty much selfexplanatory   debugger
-
-		// 	        zoom: 15,
-		// 	        center: this.myLatlng ,
-		// 	        mapTypeId: google.maps.MapTypeId.ROADMAP
-		// },
-		// myLatLng : {
-	 //      			lat: 52.3645527,
-	 //      			lng: 4.9118135
-	 //    },
-	 //    map : new google.maps.Map(document.getElementById("map-canvas"), this.mapOptions),// Attach a map to the DOM Element, with the defined settings
-	 //    marker : new google.maps.Marker({
-		// 		    position: this.myLatLng,
-		// 		    map: this.map,
-		// 		    title: 'Bijna'
-  //   			})
 	};
-	//Code forked from: http://codepen.io/dbugger/pen/LouvE/
-	// if HTML DOM Element that contains the map is found...
+
 
 	app.init();
 
-	
 
 
-	//var testData = data.requestDataPeg('https://api.wheretheiss.at/v1/' + 'satellites/25544');
 
-
-})();
+//})();
